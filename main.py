7a2ba59da3805fdc646fd637096f578cfc46a4b2
Rodrigo <leycourcino@gmail.com>
2019-07-30 17:26:53 -0500

    Commit de For Loops y White loop :icecream:

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+import sys
+
+
+
 clients = "Pablo, Ricardo"
 
 def createClient(client_name):
     global clients
+
     _addComa()
     if clientName not in clients:
         clients += client_name
         _addComa()
+        listClients()
     else:
-        print('The client already is in the client\'s list')
-
-
+        Message(0)
 
 
 def listClients():
@@ -17,30 +21,114 @@ def listClients():
     print(clients)
 
 
+def updateClient(clientName,updateClientName):
+    global clients
+
+    if clientName in clients:
+        clients = clients.replace(clientName + ',',updateClientName+',')
+        print(clientName)
+        _addComa()
+        listClients()
+    else:
+        Message(1)
+
+
+def deletedClient(clientName):
+    global clients
+
+    if clientName in clients:
+        clients = clients.replace(clientName + ',' , '')       
+        listClients()
+    else:
+        Message(0)
+      
 def _addComa():
     global clients
     clients +=  ", "
 
 
+def Message(code):
+
+    if code == 1:
+        print("The client already is in the client\'s list")#Cliente encontrado
+        
+    elif code == 2:
+        print("Client Found")
+    else:
+        print("The client is not include en Clients List")
+
+def searchClient(clientName):
+    global clients
+    clientsList = clients.split(',')
+    for client in clientsList:
+        if client != clientName:
+            continue
+        else:
+            return True    
+
+
 def _printWelcome():
-    print('*' * 50)
+    print('*' * 80)
     print("Bienvenido a Platzi Ventas")
-    print('*' * 50)
+    print('*' * 80)
     print("what would you lije to do today?")
     print("[C]Create Client")
+    print("[L]List Client")
+    print("[U]Update Client")
     print("[D]Delete Client")
+    print("[S]Search Client")
 
 
+def _getClientName():
+    clientName = None
+    
+    while not  clientName:
+        clientName = input("what is the client name? ")
+        
+        if clientName == "exit":
+            clientName = None
+            break
+    if not clientName:
+        sys.exit()
+  
+    return clientName
+
+
+    
 if __name__ == '__main__':
     _printWelcome()
 
-    command = input("")
+    command = input()
+    command = command.upper()
     if command == "C":
-        clientName = input("what is the client name?")
+        clientName = _getClientName()
         createClient(clientName)
+        
+    elif command == "L":
         listClients()
     elif command == "D":
-        pass
+         clientName = _getClientName()
+         deletedClient(clientName)
+         
+         
+    elif command =='U':
+        clientName = _getClientName()
+        updateClientName = input("what is the new name?")
+        updateClient(clientName,updateClientName)
+    
+    elif  command == "S":
+        clientName = _getClientName()
+        found = searchClient(clientName)
+
+        if found:
+            Message(2)
+            listClients()
+        else:
+            print("Bad {}".format(clientName))
+            listClients()
+
+
     else:
         print("Comando Invalido")
+        listClients()
     
